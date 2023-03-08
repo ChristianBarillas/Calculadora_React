e4f3f2c2805f426b638a6602f0ce6594b264e1a5
christian barillas <70426461+ChristianBarillas@users.noreply.github.com>
2023-03-07 19:38:24 -0600

    Initialization

---
+++ src/componets/Calculadora.jsx
@@ -0,0 +1,33 @@
+import React, {useState} from 'react'
+
+export default function Calculadora() {
+
+    const [result, setResultado] = useState('')
+
+  return (
+    <div className='contenedor'>
+
+<div className='calculadora'> 
+
+<input type='text' value={resultado} disabled />
+
+<div>
+<button onClick={limpiar}>AC</button>
+<button ></button>
+<button ></button>
+<button ></button>
+<button ></button>
+<button ></button>
+<button ></button>
+
+
+
+</div>
+
+</div>
+
+
+
+    </div>
+  )
+}
